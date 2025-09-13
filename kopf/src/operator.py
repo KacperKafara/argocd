f79f50cb3e1c8910c094e814a440b37138f463e5
Kacper Kafara <kacperkafara18@gmail.com>
2025-09-13 23:08:46 +0200

    add custom configmap

--- kopf/src/operator.py
+++ kopf/src/operator.py
@@ -1,6 +1,7 @@
 import kopf
 import pykube
 import resources_generator
+import os
 
 
 def create_image_spec(registry, tag):
@@ -12,19 +13,33 @@ def load_spec(spec):
     host = spec.get("host")
     registry = spec.get("registry")
     tag = spec.get("tag")
+    config_name = spec.get("config_name") or "nginx-config"
 
-    return host, registry, tag
+    return host, registry, tag, config_name
+
+BASE_DIR = os.path.dirname(__file__)
 
 @kopf.on.create("istio.com", "v1", "nginx")
 def on_create(spec, name, namespace, logger, body, **kwargs):
-    host, registry, tag = load_spec(spec)
+    host, registry, tag, config_name = load_spec(spec)
 
     api = pykube.HTTPClient(pykube.KubeConfig.from_env())
     image = create_image_spec(registry, tag)
-    deployment_model, deployment_name = resources_generator.create_deployment_model(image, host)
+
+    with open(os.path.join(BASE_DIR, "config", "nginx.conf")) as f:
+        nginx_config = f.read()
+
+    with open(os.path.join(BASE_DIR, "config", "mime.types")) as f:
+        mime_types = f.read()
+
+
+    deployment_model, deployment_name = resources_generator.create_deployment_model(image, host, config_name)
+    config_map = resources_generator.create_config_map({'nginx.conf': nginx_config, 'mime.types': mime_types}, config_name)
 
     kopf.append_owner_reference(deployment_model, body)
+    kopf.append_owner_reference(config_map, body)
     pykube.Deployment(api, deployment_model).create()
+    pykube.ConfigMap(api, config_map).create()
 
     logger.info(f"Created deployment model {deployment_name}")
     return {'host': host, 'image': image}
--- kopf/src/resources_generator.py
+++ kopf/src/resources_generator.py
@@ -1,4 +1,4 @@
-def create_deployment_model(image, host):
+def create_deployment_model(image, host, config_name):
     name = f"{host.split('.')[0]}-nginx"
     obj = {
         "apiVersion": "apps/v1",
@@ -26,7 +26,22 @@ def create_deployment_model(image, host):
                             "image": f"{image}",
                             "ports": [
                                 {"containerPort": 80}
+                            ],
+                            "volumeMounts": [
+                                {
+                                    "mountPath": "/etc/nginx/",
+                                    "name": f"{config_name}-volume",
+                                    "readOnly": True,
+                                }
                             ]
+                        }
+                    ],
+                    "volumes": [
+                        {
+                            "name": f"{config_name}-volume",
+                            "configMap": {
+                                "name": f"{config_name}"
+                            }
                         }
                     ]
                 }
@@ -34,3 +49,18 @@ def create_deployment_model(image, host):
         }
     }
     return obj, name
+
+
+def create_config_map(content, name):
+    configmap = {
+        "apiVersion": "v1",
+        "kind": "ConfigMap",
+        "metadata": {
+            "name": f"{name}",
+        },
+        "data": {
+            "nginx.conf": content['nginx.conf'],
+            "mime.types": content['mime.types']
+        }
+    }
+    return configmap
