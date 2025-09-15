ed3e6483b9334e03f2f961460fb98d08f66f74ed
Kacper Kafara <kacperkafara18@gmail.com>
2025-09-15 19:58:02 +0200

    simplify

--- kopf/src/operator.py
+++ kopf/src/operator.py
@@ -1,49 +1,11 @@
 import kopf
 import pykube
-import resources_generator
-import os
 
-
-def create_image_spec(registry, tag):
-    registry = f"{registry}/" if registry else ""
-    tag = tag or "latest"
-    return f"{registry}nginx:{tag}"
-
-def load_spec(spec):
-    host = spec.get("host")
-    registry = spec.get("registry")
-    tag = spec.get("tag")
-    config_name = spec.get("config_name") or "nginx-config"
-    ports = spec.get("ports") or [80]
-
-    return host, registry, tag, config_name, ports
-
-BASE_DIR = os.path.dirname(__file__)
 
 @kopf.on.create("istio.com", "v1", "nginx")
 def on_create(spec, name, namespace, logger, body, **kwargs):
-    host, registry, tag, config_name, ports = load_spec(spec)
+    namespaces = spec.get("namespaces")
 
     api = pykube.HTTPClient(pykube.KubeConfig.from_env())
-    image = create_image_spec(registry, tag)
 
-    with open(os.path.join(BASE_DIR, "config", "nginx.conf")) as f:
-        nginx_config = f.read()
-
-    with open(os.path.join(BASE_DIR, "config", "mime.types")) as f:
-        mime_types = f.read()
-
-
-    deployment_model, deployment_name = resources_generator.create_deployment_model(image, host, config_name)
-    config_map = resources_generator.create_config_map({'nginx.conf': nginx_config, 'mime.types': mime_types}, config_name)
-    service = resources_generator.create_service(ports)
-
-    kopf.append_owner_reference(deployment_model, body)
-    kopf.append_owner_reference(config_map, body)
-    kopf.append_owner_reference(service, body)
-    pykube.Deployment(api, deployment_model).create()
-    pykube.ConfigMap(api, config_map).create()
-    pykube.Service(api, service).create()
-
-    logger.info(f"Created deployment model {deployment_name}")
-    return {'host': host, 'image': image}
+    return {'namespaces': namespaces}
