40d038f00bc2e2a4d7f7c01508a5ee340e37fa00
Kacper Kafara <kacperkafara18@gmail.com>
2025-10-08 21:02:36 +0200

    setup minimal

--- kopf/src/App.py
+++
@@ -1,7 +0,0 @@
-class App:
-    def __init__(self, name, containers):
-        self.name = name
-        self.containers = containers
-
-    def __str__(self):
-        return f"App name: {self.name}, Containers: {self.containers}"
